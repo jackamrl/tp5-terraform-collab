d00548dfa4fdaafc12640876b39cce8b3758d1e4
Jacques Boyodi <jacquesboyodi@yahoo.fr>
2025-10-17 14:37:38 +0000

    (feat) : main setup

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate-rg"
+    storage_account_name  = "azerty1234azerty"
+    container_name        = "tfstate"
+    key                   = "rg-demo/terraform.tfstate"
+    use_azuread_auth      = true
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo"
+  location = "France Central"
+}
